6fd2093ed407c63b588faa7e1bd72c2b5d5e8cff
Aaron Janke <janke.aaron@gmail.com>
2018-12-19 19:55:14 -0500

    update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { store } from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/ylcQuizzes">
       <App />
     </BrowserRouter>
   </Provider>,
